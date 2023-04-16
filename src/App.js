267aa52e80a4b00152b75e0d1b34d401e7a0ec8e
Peter McClelland <peterwmcclelland@gmail.com>
2023-04-16 16:48:15 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+                <div className="navbar">
+                    <div>
+                        <NavLink to="/" style={({ isActive }) => ({ 
+                            color: isActive ? 'lightblue' : 'beige' })}>
+                            House Coffee
+                        </NavLink>
+                    </div>
+                    <div></div>
+                    <div className="navlink">
+                        <NavLink to="/about" style={({ isActive }) => ({ 
+                            color: isActive ? 'lightblue' : 'beige' })}>
+                            About
+                        </NavLink>
+                        </div>
+                </div>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+function Home() {
+  return (
+	<body>
+	<ul class="backgroundslides">
+		<li><span></span></li>
+   	<li><span></span></li>
+    <li><span></span></li>
+    <li><span></span></li>
+    <li><span></span></li>
+    <li><span></span></li>
+    </ul>
+</body>
+  )
+}
+
+export default Home;
